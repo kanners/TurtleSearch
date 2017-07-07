d9f51a22c456ac65a52ab6f49a17327cf7141871
Darrien Kennedy <darrien.kennedy@yahoo.com>
2017-07-07 12:44:54 -0400

    Added side bar to the project.

---
+++ app/src/main/java/midatlandroid/final_project/FragmentSettings.java
@@ -0,0 +1,30 @@
+package midatlandroid.final_project;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import midatlandroid.final_project.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentSettings extends Fragment {
+
+
+    public FragmentSettings() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+}
